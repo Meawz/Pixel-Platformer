e9ea25fd1282394aab01e6367ee0cbe04194dab7
Meawz <123901325+Meawz@users.noreply.github.com>
2023-11-18 01:30:15 +0200

    Add files via upload

---
+++ PlayerMovementData.gd
@@ -0,0 +1,9 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var SPEED = 100.0
+@export var ACCElERATION = 600.0
+@export var FRICTION = 1000.0
+@export var JUMP_VELOCITY = -300.0
+@export var GRAVITY_SCALE = 1.0
+@export var AIR_RESISTANCE = 200.0
---
+++ player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+@export var movement_data : PlayerMovementData
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
+	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * movement_data.GRAVITY_SCALE * delta
+
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = movement_data.JUMP_VELOCITY
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.JUMP_VELOCITY / 2:
+			velocity.y = movement_data.JUMP_VELOCITY / 2
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.FRICTION * delta)
+
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.AIR_RESISTANCE * delta)
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.SPEED * input_axis, movement_data.ACCElERATION * delta)
+
+func update_animations(input_axis):
+	if input_axis !=0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
