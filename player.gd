f3074b48f4892f64f81dfca83b083a49c6e70946
Meawz <maftei.cristian23@gmail.com>
2023-11-20 13:29:36 +0200

    Added description to code

--- player.gd
+++ player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
 
-var air_jump = false
+var air_jump = false 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
 
-func _physics_process(delta):
+func _physics_process(delta): # Where to call all functions of the character
 	apply_gravity(delta)
 	handle_jump()
 	handle_wall_jump()
@@ -25,11 +25,11 @@ func _physics_process(delta):
 	if just_left_ledge:
 		coyote_jump_timer.start()
 
-func apply_gravity(delta):
+func apply_gravity(delta): # Applies gravity to the character
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.GRAVITY_SCALE * delta
 
-func handle_wall_jump():
+func handle_wall_jump(): # Gives the ability to wall jump
 	if not is_on_wall_only(): return
 	var wall_normal = get_wall_normal()
 	if Input.is_action_just_pressed("ui_left") and wall_normal == Vector2.LEFT:
@@ -39,7 +39,7 @@ func handle_wall_jump():
 		velocity.x = wall_normal.x * movement_data.SPEED
 		velocity.y = movement_data.JUMP_VELOCITY
 
-func handle_jump():
+func handle_jump(): # Gives the ability to jump and double-jump
 	if is_on_floor() : air_jump = true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
@@ -53,7 +53,7 @@ func handle_jump():
 			velocity.y = movement_data.JUMP_VELOCITY * 0.8
 			air_jump = false
 
-func apply_friction(input_axis, delta):
+func apply_friction(input_axis, delta): # Handles the speed at which the character stops and changes direction
 	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, movement_data.FRICTION * delta)
 
@@ -61,17 +61,17 @@ func apply_air_resistance(input_axis, delta):
 	if input_axis == 0 and not is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, movement_data.AIR_RESISTANCE * delta)
 
-func handle_acceleration(input_axis, delta):
+func handle_acceleration(input_axis, delta): # Handles the acceleration of speed of the character while on floor
 	if not is_on_floor(): return 
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.SPEED * input_axis, movement_data.ACCElERATION * delta)
 
-func handle_air_acceleration(input_axis, delta):
+func handle_air_acceleration(input_axis, delta): # Handles the acceleration of speed of the character while in the air
 	if is_on_floor(): return
 	if input_axis !=0:
 		velocity.x = move_toward(velocity.x, movement_data.SPEED * input_axis, movement_data.AIR_ACCELERATION * delta)
 
-func update_animations(input_axis):
+func update_animations(input_axis): # Plays the correct animation based on the state of the character
 	if input_axis !=0:
 		animated_sprite_2d.flip_h = (input_axis < 0)
 		animated_sprite_2d.play("run")
@@ -82,5 +82,5 @@ func update_animations(input_axis):
 		animated_sprite_2d.play("jump")
 
 
-func _on_hazard_detector_area_entered(area):
+func _on_hazard_detector_area_entered(area): # Resets the player to the starting position after entering the hazard zone
 	global_position = starting_position
