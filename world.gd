21735f0a1816200d92cee732ebb4a46bddf33bcb
Meawz <maftei.cristian23@gmail.com>
2023-11-23 22:40:26 +0200

    Added Start Menu

--- world.gd
+++ world.gd
@@ -5,7 +5,6 @@ extends Node2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
-	RenderingServer.set_default_clear_color(Color.DARK_SLATE_GRAY) # Sets the color of the background
 	Events.level_completed.connect(show_level_completed)
 
 func show_level_completed(): # Displays "Level Completed!" screen after collecting all Intel in the level
