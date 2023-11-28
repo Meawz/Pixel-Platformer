fb824b8d2b03d068d7767ad7770dc35fb4ab898f
Meawz <maftei.cristian23@gmail.com>
2023-11-28 18:11:33 +0200

    Moddified the wall jumping method

--- player.gd
+++ player.gd
@@ -2,12 +2,15 @@ extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
 
-var air_jump = false 
+var air_jump = false
+var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var was_wall_normal = Vector2.ZERO
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
+@onready var wall_jump_timer = $WallJumpTimer
 
 func _physics_process(delta): # Where to call all functions of the character
 	apply_gravity(delta)
@@ -20,27 +23,35 @@ func _physics_process(delta): # Where to call all functions of the character
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall:
+		was_wall_normal = get_wall_normal()
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
+	just_wall_jumped = false
+	var just_left_wall = was_on_wall and not is_on_wall_only()
+	if just_wall_jumped:
+		wall_jump_timer.start()
+
 
 func apply_gravity(delta): # Applies gravity to the character
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.GRAVITY_SCALE * delta
 
 func handle_wall_jump(): # Gives the ability to wall jump
-	if not is_on_wall_only(): return
+	if not is_on_wall_only() and wall_jump_timer.time_left <= 0.0: return
 	var wall_normal = get_wall_normal()
-	if Input.is_action_just_pressed("move_left") and wall_normal == Vector2.LEFT:
+	if wall_jump_timer.time_left > 0.0:
+		wall_normal = was_wall_normal
+	if Input.is_action_just_pressed("jump"):
 		velocity.x = wall_normal.x * movement_data.SPEED
 		velocity.y = movement_data.JUMP_VELOCITY
-	if Input.is_action_just_pressed("move_right") and wall_normal == Vector2.RIGHT:
-		velocity.x = wall_normal.x * movement_data.SPEED
-		velocity.y = movement_data.JUMP_VELOCITY
-
+		just_wall_jumped = true
+		
 func handle_jump(): # Gives the ability to jump and double-jump
-	if is_on_floor() : air_jump = true
+	if is_on_floor(): air_jump = true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
@@ -49,7 +60,7 @@ func handle_jump(): # Gives the ability to jump and double-jump
 		if Input.is_action_just_released("jump") and velocity.y < movement_data.JUMP_VELOCITY / 2:
 			velocity.y = movement_data.JUMP_VELOCITY / 2
 			
-		if Input.is_action_just_pressed("jump") and air_jump:
+		if Input.is_action_just_pressed("jump") and air_jump and not just_wall_jumped:
 			velocity.y = movement_data.JUMP_VELOCITY * 0.8
 			air_jump = false
 
