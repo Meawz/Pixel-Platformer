a4ab1cdbf66e5fe2f53d870e314c370be9095555
Meawz <maftei.cristian23@gmail.com>
2023-12-06 21:06:04 +0200

    Small UI Bug Fix

--- player.gd
+++ player.gd
@@ -21,7 +21,6 @@ func _physics_process(delta): # Where to call all functions of the character
 	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
-	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
 	var was_on_wall = is_on_wall_only()
 	if was_on_wall:
@@ -31,9 +30,10 @@ func _physics_process(delta): # Where to call all functions of the character
 	if just_left_ledge:
 		coyote_jump_timer.start()
 	just_wall_jumped = false
-	var just_left_wall = was_on_wall and not is_on_wall_only()
+	var _just_left_wall = was_on_wall and not is_on_wall_only()
 	if just_wall_jumped:
 		wall_jump_timer.start()
+	update_animations(input_axis)
 
 
 func apply_gravity(delta): # Applies gravity to the character
@@ -54,7 +54,7 @@ func handle_jump(): # Gives the ability to jump and double-jump
 	if is_on_floor(): air_jump = true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_pressed("jump"):
 			velocity.y = movement_data.JUMP_VELOCITY
 	elif not is_on_floor():
 		if Input.is_action_just_released("jump") and velocity.y < movement_data.JUMP_VELOCITY / 2:
