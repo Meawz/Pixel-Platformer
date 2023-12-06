322229f88b0784580442bd9c8756d719677bbfc9
Meawz <maftei.cristian23@gmail.com>
2023-12-06 22:19:50 +0200

    Added TImer

--- world.gd
+++ world.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 @export var next_level: PackedScene
 
+var level_time = 0.0
+var start_level_msec = 0.0
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var level_time_label = %LevelTimeLabel
 
 func _ready():
 	Events.level_completed.connect(show_level_completed)
+	start_level_msec = Time.get_ticks_msec()
+
+func _process(delta):
+	level_time = Time.get_ticks_msec() - start_level_msec
+	level_time_label.text = str(level_time / 1000.0)
 
 func show_level_completed(): # Displays "Level Completed!" screen after collecting all Intel in the level
 	level_completed.show()
